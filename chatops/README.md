38bf60c144b6953cbd6bfba32cf0fb5a5ab60895
Peter Mosmans <support@go-forward.net>
2016-07-30 21:25:48 -0700

    Added ChatOps scripts

---
+++ chatops/README.md
@@ -0,0 +1,123 @@
+# Introduction
+This directory contains the ChatOps scripts, based on Hubot. It uses RocketChat and gitlab as the underlying framework (but can be modified to fit any other framework).
+
+This document describes the goal of the scripts, as well as installation instructions and their basic usage.
+
+
+## Workflow
+Scripts are ordered by workflow, not alphabetically.
+The workflow consists of
+1. setting up a repository for a quote with the PenText framework
+2. (optional: converting quickscope input to a quote)
+3. building a PDF quote
+4. setting up a repository for a pentest with the PenText framework, based on a quote
+5. (optional: converting gitlab issues to XML findings and non-findings)
+6. (optional: validating a PenText report)
+7. building a PDF report
+8. building a PDF invoice
+
+## Naming
+The scripts either take the **project name** as input, or the **repository name** (and optional namespace and branch). The project name is leading, repository names are derived from the project names: the quotation repository has `off-` as prefix, and the pentest repository will have `pen-` as prefix.
+As a rule of thumb, the handlers that are prefixed with `start` (startquote, startpentest) will take the **project name** as input. All other handlers take the **repository name** as input.
+
+Example: when the project's name is ros, then the corresponding quote repository and RocketChat channel's name will be `off-ros`.
+If this quote will result in a pentest project, then the corresponding repository and RocketChat channel will be named `pen-ros`.
+
+Note that git repository names are _lowercase_.
+
+
+# Scripts
+
+The scripts use multiple environment variables, that can be set by the user under which rosbot is running. These are
++ `GITLABCLI` :: the location of the python-gitlab command line interface (defaults to `gitlab`)
++ `GITSERVER` :: the name of the gitlab server (defaults to `gitlab.local`)
++ `GITWEB` :: the URL of the gitlab webinterface (defaults to `https://$GITSERVER`)
++ `NAMESPACE` :: the namespace of the user which is used to set up gitlab repositories (defaults to `ros`)
++ `PENTEXTREPO` :: the location of the PenText repository (defaults to `https://github.com/radicallyopensecurity/pentext`)
+
+## Prerequisites
+
+The Bash scripts use the python-gitlab command-line interface to talk to the gitlab instance. This interface can be installed using `sudo pip install git+https://github.com/gpocentek/python-gitlab`. Obviously, Python needs to be installed as well.
+This command line interface expects a configuration file `.python-gitlab.cfg` for the user under which rosbot is running, which it uses to connect to gitlab. Make sure it contains the correct details so that you can connect to gitlab.
+
+If you want to convert and build documents, the pentext toolchain is necessary. Use the ansible playbook https://galaxy.ansible.com/PeterMosmans/docbuilder/ or install the tools (Java, Saxon and Apache FOP) by hand, see https://github.com/radicallyopensecurity/pentext/blob/master/xml/doc/Tools%20manual.md for more information.
+
+## Test the configuration
+Test out whether the configuration is successful by manually executing the Bash script [bash/test_pentext](bash/test_pentext) - this should return an OK.
+
+
+## CoffeeScript
+
+### rosbot.coffee
+
+[scripts/rosbot.coffee](scripts/rosbot.coffee) - contains the various keywords and redirects to the proper handlers. All RocketChat-specific actions (e.g. the creation of rooms) is being handled by this script.
+
+The scripts contains an array of users that will be added to the newly created rooms by default.
+
+Example:
+`admins = ['admin']`
+
+## Bash
+
+### startquote
+Start the quotation process by setting up a repository with the PenText framework, and creating a RocketChat channel.
+
+Handled by [bash/handler_quote](bash/handler_quote)
+
+Sets up a pentest RocketChat channel named `off-PROJECT_NAME`, a gitlab repo named `off-PROJECT_NAME`, and installs the latest version of the Pentext framework. Note that this uses the `PROJECT_NAME` as input, so it will automatically append the `off-` prefix.
+
+Usage: `startquote PROJECT_NAME`
+
+
+### quickscope
+Converts a quickscope (`source/quickscope.xml`) into a full-blown XML quote.
+
+Handled by [bash/handler_quickscope](bash/handler_quickscope)
+
+
+Usage: `quickscope REPO_NAME [NAMESPACE [BRANCH]]]`
+
+
+### build
+Builds PDF files from XML quotes and reports.
+
+Handled by [bash/handler_build](bash/handler_build)
+
+Usage: `build quote|report REPO_NAME [NAMESPACE [[BRANCH]] [-PARAMETERS]`
+
+
+### startpentest
+Start the pentesting process by setting up a repository with the PenText framework, adding standard gitlab labels and issues, and creating a RocketChat channel.
+
+Handled by [bash/handler_pentest](bash/handler_pentest)
+
+Sets up a pentest RocketChat channel named `pen-PROJECT_NAME`, and a gitlab repo named `pen-PROJECT_NAME`. Will use the quotation found in the corresponding `off-PROJECT_NAME` as base. Note that the prefix `pen-` is set by the `rosbot.coffee` script.
+
+Usage: `startpentest PROJECT_NAME`
+
+
+### convert
+Converts gitlab issues labeled with `finding` and `non-finding` into XML files, and adds those to the repository
+Handled by [bash/handler_convert](bash/hander_convert)
+
+Converts gitlab items to XML findings
+
+Usage: `convert REPO_NAME`
+
+
+### validate
+Validates quotes and reports.
+Handled by [bash/handler_validate](bash/handler_validate)
+
+Validates quotes and reports using the `validate_report.py` script (proper casing, spell checking, long lines, cross-checks)
+
+Usage: `validate [OPTIONAL PARAMETERS]`
+
+
+### invoice
+Builds PDF invoices from quotes.
+
+Handled by [bash/handler_invoice](bash/handler_invoice)
+
+
+Usage: `invoice REPO_NAME INVOICE_NO [NAMESPACE [[BRANCH]] [-PARAMETERS]`
---
+++ chatops/python/docbuilder.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+
+"""
+Builds PDF files from (intermediate fo and) XML files.
+
+This script is part of the PenText framework
+                           https://pentext.org
+
+   Copyright (C) 2015-2016 Radically Open Security
+                           https://www.radicallyopensecurity.com
+
+                Author(s): Peter Mosmans
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import argparse
+import os
+import subprocess
+from subprocess import PIPE
+import sys
+import textwrap
+
+
+GITREV = 'GITREV'  # Magic tag which gets replaced by the git short commit hash
+OFFERTE = 'generate_offerte.xsl'  # XSL for generating waivers
+WAIVER = 'waiver_'  # prefix for waivers
+
+
+def parse_arguments():
+    """
+    Parses command line arguments.
+    """
+    global verboseprint
+    global verboseerror
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+Builds PDF files from (intermediate fo and) XML files.
+
+Copyright (C) 2015-2016  Radically Open Security (Peter Mosmans)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.'''))
+    parser.add_argument('-c', '--clobber', action='store_true',
+                        help='overwrite output file if it already exists')
+    parser.add_argument('-date', action='store',
+                        help='the invoice date')
+    parser.add_argument('--fop-config', action='store',
+                        default='/etc/docbuilder/rosfop.xconf',
+                        help="""fop configuration file (default
+                        /etc/docbuilder/rosfop.xconf""")
+    parser.add_argument('-f', '--fop', action='store',
+                        default='../target/report.fo',
+                        help="""intermediate fop output file (default:
+                        ../target/report.fo)""")
+    parser.add_argument('--fop-binary', action='store',
+                        default='/usr/local/bin/fop',
+                        help='fop binary (default /usr/local/bin/fop')
+    parser.add_argument('-i', '--input', action='store',
+                        default='report.xml',
+                        help="""input file (default: report.xml)""")
+    parser.add_argument('-invoice', action='store',
+                        help="""invoice number""")
+    parser.add_argument('--saxon', action='store',
+                        default='/usr/local/bin/saxon/saxon9he.jar',
+                        help="""saxon JAR file (default
+                        /usr/local/bin/saxon/saxon9he.jar)""")
+    parser.add_argument('-x', '--xslt', action='store',
+                        default='../xslt/generate_report.xsl',
+                        help='input file (default: ../xslt/generate_report.xsl)')
+    parser.add_argument('-o', '--output', action='store',
+                        default='../target/report-latest.pdf',
+                        help="""output file name (default:
+                        ../target/report-latest.pdf""")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='increase output verbosity')
+    parser.add_argument('-w', '--warnings', action='store_true',
+                        help='show warnings')
+    args = parser.parse_args()
+    if args.verbose:
+        def verboseprint(*args):  # pylint: disable=missing-docstring
+            for arg in args:
+                print(arg, end="")
+            print()
+
+        def verboseerror(*args):  # pylint: disable=missing-docstring
+            for arg in args:
+                print(arg, end="", file=sys.stderr)
+            print(file=sys.stderr)
+    else:
+        verboseprint = lambda *a: None
+        verboseerror = lambda *a: None
+    return vars(parser.parse_args())
+
+
+def print_output(stdout, stderr):
+    """
+    Prints out standard out and standard err using the verboseprint function.
+    """
+    if stdout:
+        verboseprint('[+] stdout: {0}'.format(stdout))
+    if stderr:
+        verboseerror('[-] stderr: {0}'.format(stderr))
+
+
+def change_tag(fop):
+    """
+    Replaces GITREV in document by git commit shorttag.
+    """
+    cmd = ['git', 'log', '--pretty=format:%h', '-n', '1']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    shorttag, _stderr = process.communicate()
+    if not process.returncode:
+        fop_file = open(fop).read()
+        if GITREV in fop_file:
+            fop_file = fop_file.replace(GITREV, shorttag)
+            with open(fop, 'w') as new_file:
+                new_file.write(fop_file)
+            print('[+] Embedding git version information into document')
+
+
+def to_fo(options):
+    """
+    Creates a fo output file based on a XML file.
+    Returns True if successful
+    """
+    cmd = ['java', '-jar', options['saxon'],
+           '-s:' + options['input'], '-xsl:' + options['xslt'],
+           '-o:' + options['fop'], '-xi']
+    if options['invoice']:
+        cmd.append('INVOICE_NO=' + options['invoice'])
+    if options['date']:
+        cmd.append('DATE=' + options['date'])
+    process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print_output(stdout, stderr)
+    if process.returncode:
+        print_exit('[-] Error creating fo file from XML input',
+                   process.returncode)
+    else:
+        change_tag(options['fop'])
+    return True
+
+
+def to_pdf(options):
+    """
+    Creates a PDF file based on a fo file.
+    Returns True if successful
+    """
+    cmd = [options['fop_binary'], '-c', options['fop_config'], options['fop'],
+           options['output']]
+    try:
+        verboseprint('Converting {0} to {1}'.format(options['fop'],
+                                                    options['output']))
+        process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        result = process.returncode
+        print_output(stdout, stderr)
+        if result == 0:
+            print('[+] Succesfully built ' + options['output'])
+    except OSError as exception:
+        print_exit('[-] ERR: {0}'.format(exception.strerror), exception.errno)
+    return result == 0
+
+
+def print_exit(text, result):
+    """
+    Prints error message and exits with result code.
+    """
+    print(text, file=sys.stderr)
+    sys.exit(result)
+
+
+def main():
+    """
+    The main program.
+    """
+    global verboseerror
+    global verboseprint
+    result = False
+    options = parse_arguments()
+    if not os.path.isfile(options['input']):
+        print_exit('[-] Cannot find input file {0}'.
+                   format(options['input']), result)
+    try:
+        if os.path.isfile(options['output']):
+            if not options['clobber']:
+                print_exit('[-] Output file {0} already exists. '.
+                           format(options['output']) +
+                           'Use -c (clobber) to overwrite',
+                           result)
+            os.remove(options['output'])
+    except OSError as exception:
+        print_exit('[-] Could not remove/overwrite file {0} ({1})'.
+                   format(options['output'], exception.strerror), result)
+    result = to_fo(options)
+    if result:
+        if OFFERTE in options['xslt']:  # an offerte can generate multiple fo's
+            report_output = options['output']
+            verboseprint('generating separate waivers detected')
+            output_dir = os.path.dirname(options['output'])
+            fop_dir = os.path.dirname(options['fop'])
+            try:
+                for fop in [os.path.splitext(x)[0] for x in
+                            os.listdir(fop_dir) if x.endswith('fo')]:
+                    if WAIVER in fop:
+                        options['output'] = output_dir + os.sep + fop + '.pdf'
+                    else:
+                        options['output'] = report_output
+                    options['fop'] = fop_dir + os.sep + fop + '.fo'
+                    result = to_pdf(options) and result
+            except OSError as exception:
+                print_exit('[-] ERR: {0}'.format(exception.strerror),
+                           exception.errno)
+        else:
+            result = to_pdf(options)
+
+    else:
+        print_exit('[-] Unsuccessful (error {0})'.format(result), result)
+    sys.exit(not result)
+
+
+if __name__ == "__main__":
+    main()
