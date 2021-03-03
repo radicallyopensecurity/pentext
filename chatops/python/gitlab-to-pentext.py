32a48f5eda4ce09c098ec557d6b77d32317156dc
Patricia Piolon <skyanth@users.noreply.github.com>
2021-03-03 10:10:49 +0100

    pat's version of this script

--- chatops/python/gitlab-to-pentext.py
+++ chatops/python/gitlab-to-pentext.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-"""Gitlab bridge for PenText: imports and updates gitlab issues into PenText.
+"""
+Gitlab bridge for PenText: imports and updates gitlab issues into PenText
+(XML) format
 
 This script is part of the PenText framework
                            https://pentext.org
 
-   Copyright (C) 2016-2019 Radically Open Security
+   Copyright (C) 2016-2017 Radically Open Security
                            https://www.radicallyopensecurity.com
 
                 Author(s): Peter Mosmans
@@ -49,8 +51,9 @@ class BaseItem(object):
     DECLARATION = '<?xml version="1.0" encoding="utf-8"?>\n'
 
     def __init__(self, item_type):
-        if item_type not in ('finding', 'non-finding'):
-            raise ValueError('Only finding and non-finding are currently supported')
+        if item_type not in ('finding', 'non-finding', 'Finding', 'Non-finding', 'Findings', 'Non-findings'):
+            raise ValueError(
+                'Only finding and non-finding are currently supported')
         self.item_type = item_type
         self.__path = '{0}s'.format(self.item_type)
         self.root_open = '<{0}>\n'.format(self.item_type)
@@ -76,11 +79,10 @@ class BaseItem(object):
         """Serialize item to file as XML."""
         try:
             with io.open(self.filename, 'w', encoding='utf-8') as xmlfile:
-                xmlfile.write(unicode(self))
+                xmlfile.write(str(self))
                 print_line('[+] Wrote {0}'.format(self.filename))
         except IOError:
-            print('Could not write to %s', self.filename, file=sys.stderr)
-            sys.exit(-1)
+            print_error('Could not write to %s', self.filename)
 
 
 class Finding(BaseItem):
@@ -88,19 +90,24 @@ class Finding(BaseItem):
 
     def __init__(self):
         BaseItem.__init__(self, 'finding')
-        self.threat_level = 'Moderate'
-        self.finding_type = 'TODO'
-        self.status = 'new'
+        self.threatLevel = 'Unknown'
+        self.number = ''
+        self.finding_type = ''
+        self.status = 'none'
         self.description = '<p></p>'
-        self.technicaldescription = '<p></p>'
+        self.technicaldescription = ''
         self.impact = '<p></p>'
         self.recommendation = '<ul><li></li></ul>'
 
     def __str__(self):
-        """Return an XML representation of the class."""
-        self.root_open = '<finding id="{0}" threatLevel="{1}" type="{2}"' + \
-            ' status="{3}">\n'.format(self.identifier, self.threat_level,
-                                      self.finding_type, self.status)
+        """
+        Return a XML version of the class
+        """
+        self.root_open = '<finding id="{0}" threatLevel="{1}" type="{2}" number="{3}" status="{4}">\n'.format(self.identifier,
+                                                                                                              self.threatLevel,
+                                                                                                              self.finding_type,
+                                                                                                              self.number,
+                                                                                                              self.status)
         self.content = self.element('description') + \
             self.element('technicaldescription') + \
             self.element('impact') + \
@@ -115,16 +122,67 @@ class NonFinding(BaseItem):
         BaseItem.__init__(self, 'non-finding')
 
     def __str__(self):
-        """Return an XML representation of the class."""
-        self.root_open = '<non-finding id="{0}"\n'.format(self.identifier)
+        self.root_open = '<non-finding id="{0}" number="{1}">\n'.format(self.identifier, self.number)
         return BaseItem.__str__(self)
+
+
+def get_threatlevel(issue):
+    """
+    Returns string value of first threatLevel:* label in issue
+    """
+    threatLevel = next((x for x in issue.labels if 'threatlevel:' in x.lower()), "Unknown")
+    if threatLevel != "Unknown":
+        threatLevel = threatLevel.split(":", 1)[1]
+    return threatLevel
+
+
+def get_type(issue):
+    """
+    Returns string value of first type:* label in issue
+    """
+    finding_type = next((x for x in issue.labels if 'type:' in x.lower()), "")
+    if finding_type != "":
+        finding_type = finding_type.split(":", 1)[1]
+    return finding_type
+
+
+def get_status(issue):
+    """
+    Returns string value of first status:* label in issue
+    """
+    status = next((x for x in issue.labels if 'reteststatus:' in x.lower()), "none")
+    if status != "none":
+        status = status.split(":", 1)[1]
+    return status
 
 
 def from_issue(issue):
     """Parse gitlab issue and return Finding, NonFinding or None."""
-    if 'finding' in [x.lower() for x in issue.labels]:
+    if 'findings' in [x.lower() for x in issue.labels]:
         item = Finding()
         item.description = convert_text(issue.description)
+        item.threatLevel = get_threatlevel(issue)
+        item.finding_type = get_type(issue)
+        item.status = get_status(issue)
+        for note in [x for x in reversed(issue.notes.list()) if not x.system]:
+            if len(note.body.splitlines()):
+                if 'impact' in note.body.split()[0].lower():
+                    item.impact = convert_text(''.join(note.body.splitlines(True)[1:]))
+                elif 'recommendation' in note.body.split()[0].lower():
+                    item.recommendation = convert_text(''.join(note.body.splitlines(True)[1:]))
+                else:
+                    item.technicaldescription = u'{0}\n'.format(convert_text(note.body))
+    elif 'non-findings' in [x.lower() for x in issue.labels]:
+        item = NonFinding()
+        item.content = convert_text(issue.description)
+        for note in [x for x in reversed(issue.notes.list()) if not x.system]:
+            item.content += convert_text(note.body) + '\n'
+    elif 'finding' in [x.lower() for x in issue.labels]:
+        item = Finding()
+        item.description = convert_text(issue.description)
+        item.threatLevel = get_threatlevel(issue)
+        item.finding_type = get_type(issue)
+        item.status = get_status(issue)
         for note in [x for x in reversed(issue.notes.list()) if not x.system]:
             if len(note.body.splitlines()):
                 if 'impact' in note.body.split()[0].lower():
@@ -139,9 +197,11 @@ def from_issue(issue):
         for note in [x for x in reversed(issue.notes.list()) if not x.system]:
             item.content += convert_text(note.body) + '\n'
     else:
+        print(issue.labels)
         return None
     item.title = validate_report.capitalize(issue.title.strip())
-    item.identifier = 'f{0:02}-{1}'.format(issue.iid, valid_filename(item.title))
+    item.identifier = 'f{0}-{1}'.format(issue.iid, valid_filename(item.title))
+    item.number = issue.iid
     return item
 
 
@@ -157,17 +217,26 @@ def add_item(issue, options):
                    format(item.item_type, item.filename))
         return
     if options['dry_run']:
-        print_line('[+] {0}\n{1}'.format(item.filename, item))
+        print_line(' [+] {0}\n{1}'.format(item.filename, item))
     else:
         if options['y'] or ask_permission('Create file ' + item.filename):
             item.write_file()
+
+
+def parse_images(html):
+    """
+    Download any images referenced and replace the <img> src attribute with the correct path
+    """
+    # print(html)
+    return html
 
 
 def convert_text(text):
     """
     Convert (gitlab) markdown to 'XML' (actually HTML5).
     """
-    return unicode.replace(pypandoc.convert_text(text, 'html5', format='markdown_github'), '\r\n', '\n')
+    html = str.replace(pypandoc.convert_text(text, 'html5', format='markdown_github'), '\r\n', '\n')
+    return parse_images(html)
 
 
 def ask_permission(question):
@@ -179,16 +248,24 @@ def ask_permission(question):
 
 
 def list_issues(gitserver, options):
-    """List all issues for options['issues']"""
-    try:
-        for issue in gitserver.project_issues.list(project_id=options['issues'],
-                                                   per_page=999):
-            if issue.state == 'closed' and not options['closed']:
-                continue
-            add_item(issue, options)
-    except gitlab.exceptions.GitlabListError as exception:
-        print('Could not access items ({0})'.format(exception), file=sys.stderr)
-        sys.exit(-1)
+    """
+    Lists all issues for options['issues']
+    """
+    project = gitserver.projects.get(options['issues'])
+
+    issues = project.issues.list()
+    for issue in project.issues.list(per_page=999, page=1):
+        if issue.state == 'closed' and not options['closed']:
+            continue
+        add_item(issue, options)
+    for issue in project.issues.list(per_page=999, page=2):
+        if issue.state == 'closed' and not options['closed']:
+            continue
+        add_item(issue, options)
+    for issue in project.issues.list(per_page=999, page=3):
+        if issue.state == 'closed' and not options['closed']:
+            continue
+        add_item(issue, options)
 
 
 def list_projects(gitserver):
@@ -238,7 +315,8 @@ def preflight_checks(options):
     """Check if all tools are there, return gitlab.Gitlab object."""
     gitserver = None
     try:
-        gitserver = gitlab.Gitlab.from_config('remote')
+        gitserver = gitlab.Gitlab.from_config('mine')
+        #gitserver = gitlab.Gitlab(os.environ['GITINTERNAL'], private_token=os.environ['GITLAB_TOKEN'])
         gitserver.auth()
     except gitlab.config.GitlabDataError as exception:
         print('could not connect {0}'.format(exception), file=sys.stderr)
@@ -279,7 +357,7 @@ def valid_filename(filename):
     """
     result = ''
     for char in filename.strip():
-        if char in ['*', ':', '/', '.', '\\', ' ', '[', ']', '(', ')', '\'', '\"', '`']:
+        if char in ['*', ':', '/', '.', '\\', ' ', '[', ']', '(', ')', '\'', '\"']:
             if len(char) and not result.endswith('-'):
                 result += '-'
         else:
